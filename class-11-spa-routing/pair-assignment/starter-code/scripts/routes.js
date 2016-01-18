23ad1461c0ad3d345c1c0cdaa3cb9735f3f2cb9e
Ryan <rdavid8@gmail.com>
2016-01-18 12:48:07 -0800

    completedclass11todo ryan-amy

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,11 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// done: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+// done: What function do you call to activate page.js? Fire it off now, to execute
+page.base('');
+
+page('/about', aboutController.index);
+page('/articles', articlesController.index);
+page('*', articlesController.index);
+
+page();
